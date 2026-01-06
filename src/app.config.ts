90811536c6818708ca13ce009e6245cfff5866db
Jerick Signapan <jericksignapan3331@gmail.com>
2026-01-06 08:52:43 +0800

    feat: update package.json and add service worker support
    - Updated package.json to include @angular/service-worker dependency.
    - Added new icons for the Progressive Web App in the public/icons directory.
    - Created a manifest.webmanifest file for PWA configuration.
    - Modified app.config.ts to provide service worker with registration strategy.
    - Updated index.html to reflect new app title, description, and include manifest and apple-touch-icon links.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,15 +1,19 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideZonelessChangeDetection(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
     ]
 };
