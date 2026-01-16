0143c7670c9ed3e2acbf060693fb44b6df8e3e7e
Jerick Signapan <ustpaa.villanueva@ustp.edu.ph>
2026-01-16 15:24:17 +0800

    feat: update footer content to reflect new branding and link

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        PrintTipid &copy; 2026. Built with
+        <a href="https://ric-portfolio-beta.vercel.app/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">RicAngular</a>
     </div>`
 })
 export class AppFooter {}
