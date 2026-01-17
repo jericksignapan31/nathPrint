1e77af7fdd7b61eb8328b211dc9fd3e3e58c0cea
Jerick Signapan <ustpaa.villanueva@ustp.edu.ph>
2026-01-17 12:25:27 +0800

    feat: remove pricing widget component and update imports in landing page

--- src/app/pages/landing/components/topbarwidget.component.ts
+++ src/app/pages/landing/components/topbarwidget.component.ts
@@ -3,7 +3,7 @@ import { StyleClassModule } from 'primeng/styleclass';
 import { Router, RouterModule } from '@angular/router';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
-import {AppFloatingConfigurator} from "@/app/layout/component/app.floatingconfigurator";
+import { AppFloatingConfigurator } from '@/app/layout/component/app.floatingconfigurator';
 
 @Component({
     selector: 'topbar-widget',
@@ -50,16 +50,11 @@ import {AppFloatingConfigurator} from "@/app/layout/component/app.floatingconfig
                         <span>Highlights</span>
                     </a>
                 </li>
-                <li>
-                    <a (click)="router.navigate(['/landing'], { fragment: 'pricing' })" pRipple class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
-                        <span>Pricing</span>
-                    </a>
-                </li>
             </ul>
             <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2">
                 <button pButton pRipple label="Login" routerLink="/auth/login" [rounded]="true" [text]="true"></button>
                 <button pButton pRipple label="Register" routerLink="/auth/login" [rounded]="true"></button>
-                <app-floating-configurator [float]="false"/>
+                <app-floating-configurator [float]="false" />
             </div>
         </div> `
 })
